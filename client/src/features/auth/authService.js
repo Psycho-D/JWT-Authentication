70be31adb0a6ac03d8fca23ce5181c3e91b5ba6d
Ali <psychodev12@gmail.com>
2023-02-07 12:13:52 +0500

    initial commit

---
+++ client/src/features/auth/authService.js
@@ -0,0 +1,56 @@
+// services files deal with https requests
+const API_URL = "http://localhost:5000/api/users/";
+
+// Register user
+const register = async (userData) => {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    });
+
+    // console.log(response.json());
+
+    if(response.ok) {
+        const result = await response.json();
+        console.log(result);
+        localStorage.setItem("user", JSON.stringify(result));
+        return result;
+    }
+}
+
+// Login user
+const login = async (userData) => {
+    const response = await fetch(API_URL + "login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    });
+
+    // console.log(response.json());
+
+    if(response.ok) {
+        const result = await response.json();
+        console.log(result);
+        localStorage.setItem("user", JSON.stringify(result));
+        return result;
+    }
+}
+
+
+// Logout user 
+const logout = () => {
+    localStorage.removeItem("user");
+}
+
+const authService = {
+    register,
+    login,
+    logout
+}
+
+export default authService;
